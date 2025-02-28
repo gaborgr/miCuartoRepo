363f85cfa950a06915a818037e49865076afa734
gaborgr <gaborgr@gmail.com>
2025-02-28 10:28:48 -0300

    Agregando metodo cursos

--- API/app.py
+++ API/app.py
@@ -14,3 +14,8 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+@app.get("/cursosplatzi")
+def get_superheroes():
+    rows = ["Python", "Django", "Flask", "FastAPI", "JavaScript", "React", "Vue", "Angular"]
+    return rows
